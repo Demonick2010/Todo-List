3a2d32aadc51c358a8ccec02c800bb7332dc8fcf
Demonick2010 <kolyan20022002@gmail.com>
2020-08-11 10:33:51 +0300

    Add project files.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo_List.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
